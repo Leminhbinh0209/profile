cd0a06f15a8dd9f1b9d08731dace10dab0fc6572
Leminhbinh0209 <binhleminh0209@gmail.com>
2020-07-17 03:18:42 -0600

    add all

---
+++ app.js
@@ -0,0 +1,41 @@
+new fullpage("#fullpage", {
+    autoScrolling: true,
+    navigation:true,
+    afterLoad: (origin, destination, direction) => {
+        const section = destination.item;
+        const tl = new TimelineMax({delay:0.5});
+        const welcome = section.querySelector('.welcome');
+        console.log(destination);
+        if (destination.index === 0) {
+            tl.fromTo(welcome, 1, { y:"40%", height: "0%",opacity:0}, {y:"-30%", height: "30%", opacity:1, ease:Power2.easeInOut})
+            ;
+        }
+    },
+    onLeave: (origin, destination, direction) => {
+        const section = destination.item;
+        const title = section.querySelector('h1');
+        const tl = new TimelineMax({delay:0.5});
+        console.log(destination);
+        
+        if (destination.index === 1){
+            tl.fromTo(title, 0.5, {y:'50', opacity:0}, {y:0, opacity:1});
+            const dia = document.querySelectorAll('.DiaPer1');
+            const description = document.querySelectorAll('.description');
+
+            console.log(description);
+            tl.fromTo(dia, 0.35, { opacity:0}, {opacity:1})
+            .fromTo(description[0], 0.45, { opacity:0}, {opacity:1})
+            .fromTo(description[1], 0.45, { opacity:0}, {opacity:1});
+
+        }
+        if (destination.index === 2){
+            const pros = document.querySelectorAll('.projects');
+            tl.fromTo(title, 0.5, {y:'50', opacity:0}, {y:0, opacity:1});
+            const dia = document.querySelectorAll('.DiaPer2');
+            console.log(dia);
+            tl.fromTo(dia, 0.55, { x:"-750"}, {x:0})
+            .fromTo(pros,  0.45, { opacity:0}, {opacity:1});
+
+        }
+    }
+})
