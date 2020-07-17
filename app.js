dd8efc2dc34ca8b512e91c9b2cd0d97b04791bb1
Leminhbinh0209 <binhleminh0209@gmail.com>
2020-07-17 07:38:53 -0600

    boostrap update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ new fullpage("#fullpage", {
         const welcome = section.querySelector('.welcome');
         console.log(destination);
         if (destination.index === 0) {
-            tl.fromTo(welcome, 1, { y:"40%", height: "0%",opacity:0}, {y:"-30%", height: "30%", opacity:1, ease:Power2.easeInOut})
+            tl.fromTo(welcome, 1, {  height: "0%",opacity:0}, { height: "30%", opacity:1, ease:Power2.easeInOut})
             ;
         }
     },
@@ -37,10 +37,10 @@ new fullpage("#fullpage", {
             tl.fromTo(dia, 0.55, { x:"-750"}, {x:0})
             .fromTo(pros,  0.45, { opacity:0}, {opacity:1});
 
-            tl.fromTo(langpr[0], 0.35, {  height: "0%",opacity:0}, { height: "30%", opacity:1, ease:Power2.easeInOut})
-            .fromTo(langpr[1], 0.35, {height: "0%",opacity:0}, { height: "30%", opacity:1, ease:Power2.easeInOut})
-            .fromTo(langpr[2], 0.35, {  height: "0%",opacity:0}, { height: "30%", opacity:1, ease:Power2.easeInOut})
-            .fromTo(langpr[3], 0.35, {  height: "0%",opacity:0}, {height: "30%", opacity:1, ease:Power2.easeInOut})
+            tl.fromTo(langpr[0], 0.35, {  height: "0%",opacity:0}, { height: "100%", opacity:1, ease:Power2.easeInOut})
+            .fromTo(langpr[1], 0.35, {height: "0%",opacity:0}, { height: "100%", opacity:1, ease:Power2.easeInOut})
+            .fromTo(langpr[2], 0.35, {  height: "0%",opacity:0}, { height: "100%", opacity:1, ease:Power2.easeInOut})
+            .fromTo(langpr[3], 0.35, {  height: "0%",opacity:0}, {height: "100%", opacity:1, ease:Power2.easeInOut})
             ;
         }
     }
